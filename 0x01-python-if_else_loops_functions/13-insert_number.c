2a062cd2868fa441faebce11039d3fe440e4164f
Ismaila Lawal Elijah <2017en0285@unijos.edu.ng>
2022-08-23 09:11:17 -0700

    adds a node when head is NULL

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_node - inserts node in sorted list
+ * @head: address of head pointer
+ * @number: number to insert
+ * Return: inserted node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node = *head, *new = malloc(sizeof(listint_t));
+
+	if (!new)
+		return (NULL);
+
+	new->n = number;
+	new->next = NULL;
+
+	if (!node || new->n < node->n)
+	{
+		new->next = node;
+		return (*head = new);
+	}
+
+	while (node)
+	{
+		if (!node->next || new->n < node->next->n)
+		{
+			new->next = node->next;
+			node->next = new;
+			return (node);
+		}
+		node = node->next;
+	}
+	return (NULL);
+}
