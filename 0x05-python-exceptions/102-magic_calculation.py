5ea7112eda6481b55e8139b306e279a01b6fa9bf
Ismaila Lawal Elijah <2017en0285@unijos.edu.ng>
2022-09-12 18:46:08 -0700

    the Python function def magic_calculation(a, b): that does exactly the same as the following Python bytecode:

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -8,8 +8,8 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception('Too far')
             else:
-                result += a ** b / i
+                result += (a ** b) / i
         except:
             result = b + a
             break
-    return (result)
+    return result
