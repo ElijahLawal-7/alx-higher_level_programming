c7510e419fa1328560fb419d2baac550d18451d6
Ismaila Lawal Elijah <2017en0285@unijos.edu.ng>
2022-09-20 21:31:40 -0700

    python-everything_is_object

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(l=[]):
-    l += ["Holberton"]
-    return ", ".join(l)
+def magic_string():
+    setattr(magic_string, "i", getattr(magic_string, "i", -1) + 1)
+    return "BestSchool" + ", BestSchool" * getattr(magic_string, "i", 0)
