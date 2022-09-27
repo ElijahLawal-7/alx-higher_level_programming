8dc8697ce01eba40e66ea1c75593a2a07255d4dd
Ismaila Lawal Elijah <2017en0285@unijos.edu.ng>
2022-09-26 18:10:37 -0700

    a class BaseGeometry (based on 5-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""BaseGeometry class Module"""
+
+
+class BaseGeometry():
+    """BaseGeometry class"""
+    def area(self):
+        """Raises an Exception"""
+        raise Exception("area() is not implemented")
