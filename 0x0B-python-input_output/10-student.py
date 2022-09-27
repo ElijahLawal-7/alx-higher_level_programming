6a669e8dde12bbc765c07f8195c34f4394c6a791
Ismaila Lawal Elijah <2017en0285@unijos.edu.ng>
2022-09-27 12:15:54 -0700

    a class Student that defines a student by: (based on 9-student.py)

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+file: 11-student.py
+Classes:
+-> Student
+'''
+
+
+class Student:
+    ''' Student class '''
+
+    def __init__(self, first_name, last_name, age):
+        ''' Constructor method '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        ''' Method that returns directory description with filter '''
+
+        if isinstance(attrs, list) and all(isinstance(attr, str)
+                                           for attr in attrs):
+            res = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    res[i] = self.__dict__[i]
+            return res
+        return self.__dict__
